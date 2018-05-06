59851da4ffa2b432be12ea61518583d96b9a02e4
UnknowColum <38615471+UnknowColum@users.noreply.github.com>
2018-05-06 03:38:43 +0200

    Update crosshair_client.lua

--- player_crosshair/crosshair_client.lua
+++ player_crosshair/crosshair_client.lua
@@ -1,56 +1,14 @@
-Citizen.CreateThread(function()
-    local isSniper = false
-    while true do
-        Citizen.Wait(0)
-
-        local ped = GetPlayerPed(-1)
-        local currentWeaponHash = GetSelectedPedWeapon(ped)
-
-        if currentWeaponHash == 100416529 then
-            isSniper = true
-        elseif currentWeaponHash == 205991906 then
-            isSniper = true
-        elseif currentWeaponHash == -952879014 then
-            isSniper = true
-        elseif currentWeaponHash == GetHashKey('WEAPON_HEAVYSNIPER_MK2') then
-            isSniper = true
-        else
-            isSniper = false
-        end
-
-        if not isSniper then
-            HideHudComponentThisFrame(14)
-        end
-    end
-end)
-
 local driveWanderTaskActive = false
 local driveToWpTaskActive = false
 
+
+-- function
 function ShowNotification(text)
     SetNotificationTextEntry("STRING")
     AddTextComponentString(text)
     DrawNotification(false, false)
 end
 
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        SetCanAttackFriendly(GetPlayerPed(-1), true, false)
-        NetworkSetFriendlyFireOption(true)
-    end
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        if IsControlJustReleased(0, 56) then
-            DriveToWp()
-        elseif IsControlJustReleased(0, 57) then
-            DriveWander()
-        end
-    end
-end)
 
 function DriveToWp()
     local PlayerPed = GetPlayerPed(-1)
@@ -85,3 +43,48 @@ function DriveWander()
         TaskVehicleDriveWander(PlayerPed, veh, 16.0, 786603)
     end
 end
+
+-- Boucle
+
+Citizen.CreateThread(function()
+    local isSniper = false
+    while true do
+        Citizen.Wait(0)
+        local ped = GetPlayerPed(-1)
+        local currentWeaponHash = GetSelectedPedWeapon(ped)
+        if currentWeaponHash == 100416529 then
+            isSniper = true
+        elseif currentWeaponHash == 205991906 then
+            isSniper = true
+        elseif currentWeaponHash == -952879014 then
+            isSniper = true
+        elseif currentWeaponHash == GetHashKey('WEAPON_HEAVYSNIPER_MK2') then
+            isSniper = true
+        else
+            isSniper = false
+        end
+        if not isSniper then
+            HideHudComponentThisFrame(14)
+        end
+    end
+end)
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        SetCanAttackFriendly(GetPlayerPed(-1), true, false)
+        NetworkSetFriendlyFireOption(true)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustReleased(0, 56) then
+            DriveToWp()
+        elseif IsControlJustReleased(0, 57) then
+            DriveWander()
+        end
+    end
+end)
