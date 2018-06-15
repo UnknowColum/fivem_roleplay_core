f8642da4e0cceefc429d98ebcc9ce4079d0f317e
Dylan Malandain <lucenzo.76@gmail.com>
2018-06-15 02:48:32 +0200

    Clear

--- configuration.lua
+++
@@ -1,4 +0,0 @@
-fivem_roleplay_core = {
-    _pvp_status = true,
-    _wanted_ia = false,
-}
--- player_pvp/server_pvp.lua
+++
@@ -1,9 +0,0 @@
-RegisterNetEvent('five_roleplay_core:pvp_status')
-AddEventHandler('five_roleplay_core:pvp_status', function()
-    local source = source
-    if fivem_roleplay_core._pvp_status == 'true' then
-        TriggerClientEvent('five_roleplay_core:pvp_status_true', source)
-    else
-        TriggerClientEvent('five_roleplay_core:pvp_status_false', source)
-    end
-end)
